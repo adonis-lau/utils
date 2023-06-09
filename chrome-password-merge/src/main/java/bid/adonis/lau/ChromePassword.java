d6b2fa47a6fb6b4e3b06cfae27ea1840b01ebe48
adonis.lau <adonis.lau.dev@gmail.com>
2023-06-09 18:56:28 +0800

    chrome password merge

---
+++ chrome-password-merge/src/main/java/bid/adonis/lau/ChromePassword.java
@@ -0,0 +1,33 @@
+package bid.adonis.lau;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @author adonis lau
+ * @date 2023/6/9 18:33
+ */
+@Slf4j
+public class ChromePassword {
+    public static void main(String[] args) {
+        String init = "/Users/adonis/Library/CloudStorage/Dropbox/Applications/Syncs/1Password/ChromePassword.csv";
+        String output = "/Users/adonis/Downloads/ChromePassword.xlsx";
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(init));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] fields = line.split(",");
+                // 处理每一行数据
+                log.info(JSONObject.toJSONString(fields));
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
